ce3896409a915e226c4a8f6239db68b77279eb16
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-06 13:34:45 +0300

    Add quote page

---
+++ src/home/quotePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuoteComponent from '../QuoteComponent/quote';
+
+function Quote() {
+  return (
+    <div>
+      <QuoteComponent />
+    </div>
+  );
+}
+
+export default Quote;
