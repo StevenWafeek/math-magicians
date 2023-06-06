fdf442f8abe39b3df601bdedc1e471462a166428
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-06 13:32:31 +0300

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+// import './App.css';
+// import Calculator from './components/Calculator';
+// import QuoteComponent from './QuoteComponent/quote';
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Math-magicians</h1>
+//       <Calculator />
+//       <QuoteComponent />
+//     </div>
+
+//   );
+// }
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
-import QuoteComponent from './QuoteComponent/quote';
+import Navigation from './home/navigation';
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Calculator</h1>
-      <Calculator />
-      <QuoteComponent />
-    </div>
-
+    <Router>
+      <div className="App">
+        <Navigation />
+      </div>
+    </Router>
   );
 }
+
+export default App;
