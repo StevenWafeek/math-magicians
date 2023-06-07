fc7eec0092c829adea1295c2211513938e8d5113
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-07 23:45:23 +0300

    Fix tests files

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import  { render } from '@testing-library/react';
-import Calculator from './Calculator';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { prettyDOM } from '@testing-library/dom';
+import Calculator from './Calculator';
 
 describe('Calculator', () => {
- const calculator = render(<Calculator />)
-test('calculator is rendering',() => {
-    expect(prettyDOM(calculator.container)).toMatchSnapshot()
-});
-
+  const calculator = render(<Calculator />);
+  test('calculator is rendering', () => {
+    expect(prettyDOM(calculator.container)).toMatchSnapshot();
+  });
 });
--- src/components/chiled.test.js
+++ src/components/chiled.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import  { render } from '@testing-library/react';
-import Display from './chiled';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { prettyDOM } from '@testing-library/dom';
+import Display from './chiled';
 
 describe('Calculator', () => {
- const calculator = render(<Display />)
-test('calculator is rendering',() => {
-    expect(prettyDOM(calculator.container)).toMatchSnapshot()
-});
-
+  const calculator = render(<Display />);
+  test('calculator is rendering', () => {
+    expect(prettyDOM(calculator.container)).toMatchSnapshot();
+  });
 });
